40a0a45c36037fa2911b25b80c54f975ff3a207d
Erik Rosness <erik@rosness.no>
2020-03-27 08:50:33 +0100

    Added general POST handler for heat.

--- app/api/heat.mjs
+++ app/api/heat.mjs
@@ -1,9 +1,19 @@
 import { getSimulator } from '../access/simulator.mjs'
 import express from 'express'
 
-export function general( req, res ) {
+export function generalGet( req, res ) {
   const splitPath = req.path.split('/')
-  console.log("At general:", splitPath)
+  console.log("At generalGet:", splitPath)
+  const responseObject = getSimulator(splitPath[3],splitPath[4])
+  if (responseObject == undefined) {
+    res.sendStatus(404)
+  }
+  res.send(JSON.stringify(responseObject))
+}
+
+export function generalPost( req, res ) {
+  const splitPath = req.path.split('/')
+  console.log("At generalPost:", splitPath)
   const responseObject = getSimulator(splitPath[3],splitPath[4])
   if (responseObject == undefined) {
     res.sendStatus(404)
