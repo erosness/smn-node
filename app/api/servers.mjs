8400e49cdb1c2965c1bdc7a319e6089ddffb364b
Erik Rosness <erik@rosness.no>
2020-05-25 08:56:21 +0200

    Get rid of pairId as object entry visible outside node. This one needs a cleanup when real discovery is estyablished.

--- app/api/servers.mjs
+++ app/api/servers.mjs
@@ -3,16 +3,16 @@ import { getPairId } from '../service/node'
 
 export function servers( req, res ) {
   res.json([
-    { ip: getIp(), port: 5066, pairId: getPairId("5066") },
-    { ip: getIp(), port: 5067, pairId: getPairId("5067") },
-    { ip: getIp(), port: 5068, pairId: getPairId("5068") },
-    { ip: getIp(), port: 5069, pairId: getPairId("5069") },
-    { ip: getIp(), port: 5071, pairId: getPairId("5071") },
-    { ip: getIp(), port: 5072, pairId: getPairId("5072") },
-    { ip: getIp(), port: 5073, pairId: getPairId("5073") },
-    { ip: getIp(), port: 5074, pairId: getPairId("5074") },
-    { ip: getIp(), port: 5075, pairId: getPairId("5075") },
-    { ip: getIp(), port: 5076, pairId: getPairId("5076") },
-    { ip: getIp(), port: 5077, pairId: getPairId("5077") }
+    { ip: getIp(), port: 5066, uid: getPairId("5066") },
+    { ip: getIp(), port: 5067, uid: getPairId("5067") },
+    { ip: getIp(), port: 5068, uid: getPairId("5068") },
+    { ip: getIp(), port: 5069, uid: getPairId("5069") },
+    { ip: getIp(), port: 5071, uid: getPairId("5071") },
+    { ip: getIp(), port: 5072, uid: getPairId("5072") },
+    { ip: getIp(), port: 5073, uid: getPairId("5073") },
+    { ip: getIp(), port: 5074, uid: getPairId("5074") },
+    { ip: getIp(), port: 5075, uid: getPairId("5075") },
+    { ip: getIp(), port: 5076, uid: getPairId("5076") },
+    { ip: getIp(), port: 5077, uid: getPairId("5077") }
   ])
 }
